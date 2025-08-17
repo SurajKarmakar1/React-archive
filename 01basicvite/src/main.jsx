bf00166f94a49d0634237347cd2f2272acdff3c0
surajtheneko07 <nekodev1807@gmail.com>
2025-08-17 20:27:01 +0530

    Implement custom React and integrate with Vite app

--- 01basicvite/src/main.jsx
+++ 01basicvite/src/main.jsx
@@ -1,9 +1,48 @@
-import { StrictMode } from "react";
+import React from "react";
 import { createRoot } from "react-dom/client";
 import App from "./App.jsx";
 
+const reactElement = {
+  //custom declaration so react dont under this
+  type: "a",
+  props: {
+    target: "_blank",
+    href: "https://react.dev/",
+  },
+
+  children: "click me to visit react",
+};
+
+function MyApp() {
+  return (
+    <>
+      //jsx
+      <h1>Vite react app</h1>
+      <h2>example</h2>
+    </>
+  );
+}
+
+const AnotherElement = (
+  <a href="https://google.com" target="_blank">
+    Visit Google
+  </a>
+);
+
+const areactElement = React.createElement(
+  //parameter order: element , attributes, children
+  "a",
+
+  { href: "https://google.com", target: "_blank" },
+
+  "click to visit google"
+);
+
 createRoot(document.getElementById("root")).render(
-  <StrictMode>
-    <App />
-  </StrictMode>
+  // <App />    // this is jsx syntax for rendering a react component
+  //   <MyApp />
+  //reactElement               // this wont work as react dont understand this.
+  //AnotherElement
+
+  areactElement //This refers directly to the value stored in the areactElement variable.
 );
