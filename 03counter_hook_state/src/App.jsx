b1d9deeb659e34351966d7d7b2a481248c493477
surajtheneko07 <nekodev1807@gmail.com>
2025-08-19 01:34:16 +0530

    update

--- 03counter_hook_state/src/App.jsx
+++ 03counter_hook_state/src/App.jsx
@@ -1,35 +1,37 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-
+import { useState } from "react";
+import "./App.css";
+//any change in the state wil re render the web page.(why state was designed for in react.)
 function App() {
-  const [count, setCount] = useState(0)
+  // let counter = 0;
+  let [counter, setCounter] = useState(0); // hooks
 
+  const addValue = () => {
+    setCounter((prevCounter) => {
+      prevCounter + 1;
+    });
+    setCounter((prevCounter) => {
+      prevCounter + 1;
+    });
+    setCounter((prevCounter) => {
+      prevCounter + 1;
+    });
+    setCounter((prevCounter) => {
+      prevCounter + 1;
+    });
+  };
+
+  const removeValue = () => {
+    setCounter(counter - 1);
+  };
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <h1>React Practice: {counter}</h1>
+      <h2>Counter value: {counter}</h2>
+      <button onClick={addValue}>Add value</button>{" "}
+      <button onClick={removeValue}>remove value</button>
+      <p>footer:{counter}</p>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
